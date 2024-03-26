c07c17f11a606fb74232a314c0376468ebad4322
fullsunhcc <atlantic092407@gmail.com>
2024-03-26 10:55:12 +0900

    Solved the BEV map code

--- sfa/data_process/kitti_bev_utils.py
+++ sfa/data_process/kitti_bev_utils.py
@@ -30,9 +30,11 @@ def add_v_labels(lidar_points):
 
 def makeBEVMap(PointCloud_,Camera_,Calib_, boundary):
     t.tic()
+    # BEV_HEIGHT = 607 , BEV_WIDTH = 607 
     Height = cnf.BEV_HEIGHT + 1
     Width = cnf.BEV_WIDTH + 1
-
+    
+    # This is Transfrom velodyne data (lidar data) to Camera coordinate
     png = np.copy(Camera_)
     calib = Calibration(Calib_)
     P2 = calib.P2
@@ -41,7 +43,6 @@ def makeBEVMap(PointCloud_,Camera_,Calib_, boundary):
     R0_rect = np.insert(R0_rect,3,values=[0,0,0,1],axis=1)
     Tr_velo_to_cam = calib.V2C
     Tr_velo_to_cam = np.insert(Tr_velo_to_cam,3,values=[0,0,0,1],axis=0)
-
     scan = PointCloud_[:, :4].copy()
     scan[:, 2] -= 2.73
     velo = np.insert(scan,3,1,axis=1).T
@@ -49,11 +50,11 @@ def makeBEVMap(PointCloud_,Camera_,Calib_, boundary):
     cam_labeled = add_v_labels(cam)
     cam_labeled = np.delete(cam_labeled,np.where(cam_labeled[2,:]<=0),axis=1)
 
-    # get u,v,z
+    # get u,v,z Camera coordinate
     cam_labeled[:2] /= cam_labeled[2,:]
     IMG_H,IMG_W,_ = png.shape
 
-    # filter point out of canvas
+    # filter lidar point out of canvas
     u, v, _ = cam_labeled[:3, :]
     valid_indices = (
         (u > 0) & (u < IMG_W) & (v > 0) & (v < IMG_H)
@@ -69,79 +70,93 @@ def makeBEVMap(PointCloud_,Camera_,Calib_, boundary):
     PointCloud = np.copy(PointCloud_)
     PointCloud[:, 0] = np.int_(np.floor(PointCloud[:, 0] / cnf.DISCRETIZATION))
     PointCloud[:, 1] = np.int_(np.floor(PointCloud[:, 1] / cnf.DISCRETIZATION) + Width / 2)
-    PointCloud[:, 2] += 0.0001
+    PointCloud[:, 2] += 0.0001 # To prevent zero
     labels = np.arange(len(PointCloud))
     PointCloud = np.c_[PointCloud, labels]
     selected = PointCloud[np.isin(PointCloud[:, 4], labeled)]
+    # Add point cloud data with rgb_values
     selected_points = np.hstack((selected,rgb_values))
     
-    # sort-3times for Height, Intensity & Density
+    # sort-3times for Height, Intensity and  Density
     sorted_indices = np.lexsort((-PointCloud[:, 2], PointCloud[:, 1], PointCloud[:, 0]))
     PointCloud = PointCloud[sorted_indices]
     _, unique_indices, unique_counts = np.unique(PointCloud[:, 0:2], axis=0, return_index=True, return_counts=True)
     PointCloud_top = PointCloud[unique_indices]
 
-    # Height Map, Intensity Map ,Density Map & Color Map
+    # Height Map, Intensity Map ,Density Map based on lidar data
     heightMap = np.zeros((Height, Width))
     intensityMap = np.zeros((Height, Width))
     densityMap = np.zeros((Height, Width))
 
     # some important problem is image coordinate is (y,x), not (x,y)
+    # This is the method of making heightMap (lidar Only)
     max_height = float(np.abs(boundary['maxZ'] - boundary['minZ']))
     heightMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 2] / max_height
-
+    
+    # This is the method of making densityMap (lidar Only)
     normalizedCounts = np.minimum(1.0, np.log(unique_counts + 1) / np.log(64))
-    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
     densityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
+
+    # This is the method of making intensityMap (lidar Only)
+    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
 
     # Create a DataFrame from the selected points
     df = pd.DataFrame(selected_points, columns=['x', 'y', 'z', 'label', 'intensity', 'r', 'g', 'b'])
 
     # Color point grid cells
-    grid_cell_size = 50/608
-    x_grid = np.arange(0, 50 , grid_cell_size)
-    y_grid = np.arange(-25, 25 , grid_cell_size)
-
-    # Add grid cell information to the DataFrame
-    df['grid_x'] = np.digitize(df['x'], x_grid) - 1
-    df['grid_y'] = np.digitize(df['y'], y_grid) - 1
-
-    # Calculate normalized values for 'z' based on their sum
-    df['normalized_z'] = df['z'] / df.groupby(['grid_x', 'grid_y'])['z'].transform('max')
-    sum_normalized_z = df.groupby(['grid_x', 'grid_y'])['normalized_z'].transform('sum')
-    sum_normalized_z = sum_normalized_z.replace(0, 0.0001)
-    df['normalized_z'] /= sum_normalized_z
+    x_grid = np.arange(0, 607 , 1)
+    y_grid = np.arange(0, 607 , 1)
     
-    exp_factor = 0.1  # Adjust this value for desired sensitivity to distance from mean
+    # Add grid cell information to the DataFrame
+    df['grid_x'] = np.digitize(df['x'], x_grid)
+    df['grid_y'] = np.digitize(df['y'], y_grid)
 
-    # Calculate the normalized exponential values for 'intensity' within each grid cell
-    df['normalized_intensity'] = df['intensity'].groupby([df['grid_x'], df['grid_y']]).transform(lambda x: np.exp(-exp_factor * np.abs(x - x.mean())))
-    sum_normalized_intensity = df.groupby(['grid_x', 'grid_y'])['normalized_z'].transform('sum')
-    sum_normalized_intensity = sum_normalized_intensity.replace(0, 0.0001)
-    df['normalized_intensity'] /= sum_normalized_intensity
+    # Perform grouped operations to minimize the number of groupby calls
+    grouped = df.groupby(['grid_x', 'grid_y'])
 
-    # Calculate the intermediate values within each group
-    df['Zr+Ir'] = df['r'] * (densityMap[df['grid_x'], df['grid_y']] * df['normalized_intensity'] + (1-densityMap[df['grid_x'], df['grid_y']])*df['normalized_z'])
-    df['Zg+Ig'] = df['g'] * (densityMap[df['grid_x'], df['grid_y']] * df['normalized_intensity'] + (1-densityMap[df['grid_x'], df['grid_y']])*df['normalized_z'])
-    df['Zb+Ib'] = df['b'] * (densityMap[df['grid_x'], df['grid_y']] * df['normalized_intensity'] + (1-densityMap[df['grid_x'], df['grid_y']])*df['normalized_z'])
-    # Calculate grouped means
+    # Calculate normalized height within each grid cell
+    max_z_per_group = grouped['z'].transform('max')
+    df['normalized_z'] = df['z'] / max_z_per_group
+    df['normalized_z'] /= grouped['normalized_z'].transform('sum').replace(0, 0.0001)
+    # Hyperparameter for weighting the value based on proximity to the mean intensity
+    exp_factor = 0.1
+
+    # Calculate normalized intensity within each grid cell
+    mean_intensity_per_group = grouped['intensity'].transform('mean')
+    df['normalized_intensity'] = np.exp(-exp_factor * np.abs(df['intensity'] - mean_intensity_per_group))
+    df['normalized_intensity'] /= grouped['normalized_intensity'].transform('sum').replace(0, 0.0001)
+
+    # Precompute density factors for optimization
+    density_factor = densityMap[df['grid_x'], df['grid_y']]
+    normalized_intensity_factor = (1 - density_factor) * df['normalized_intensity']
+    normalized_z_factor = density_factor * df['normalized_z']
+
+    # Calculate the intermediate RGB values using precomputed factors
+    df['Zr+Ir'] = df['r'] * (normalized_intensity_factor + normalized_z_factor)
+    df['Zg+Ig'] = df['g'] * (normalized_intensity_factor + normalized_z_factor)
+    df['Zb+Ib'] = df['b'] * (normalized_intensity_factor + normalized_z_factor)
+
+    # Calculate grouped means of grid cell R,G,B seperately
     grouped_means = df.groupby(['grid_x', 'grid_y'])[['Zr+Ir', 'Zg+Ig', 'Zb+Ib']].mean().reset_index()
 
+    # Making the grid cell size for the (lidar + RGB) based rgb_map 
     rMap = np.zeros((Height - 1, Width - 1))
     bMap = np.zeros((Height - 1, Width - 1))
     gMap = np.zeros((Height - 1, Width - 1))
     
+    # Put the calculated r, g, b value which calculated by the on the right coordinate of rgb_map
     rMap[grouped_means['grid_x'], grouped_means['grid_y']] = grouped_means['Zr+Ir']
     gMap[grouped_means['grid_x'], grouped_means['grid_y']] = grouped_means['Zg+Ig']
     bMap[grouped_means['grid_x'], grouped_means['grid_y']] = grouped_means['Zb+Ib']
-
+    
+    # Making the grid 6 channels RGB_Map to give for YOLO architecture
     RGB_Map = np.zeros((6, Height -1 , Width -1 ))
-    RGB_Map[5, :, :] = rMap
-    RGB_Map[4, :, :] = gMap
-    RGB_Map[3, :, :] = bMap
-    RGB_Map[2, :, :] = densityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # r_map
-    RGB_Map[1, :, :] = heightMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # g_map
-    RGB_Map[0, :, :] = intensityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # b_map
+    RGB_Map[5, :, :] = rMap # (lidar + RGB) based r_map
+    RGB_Map[4, :, :] = gMap # (lidar + RGB) based b_map
+    RGB_Map[3, :, :] = bMap  # (lidar + RGB) based g_map
+    RGB_Map[2, :, :] = densityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # lidar r_map
+    RGB_Map[1, :, :] = heightMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # lidar g_map
+    RGB_Map[0, :, :] = intensityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # lidar b_map
     t.toc()
     return RGB_Map
 
@@ -172,7 +187,7 @@ def get_corners(x, y, w, l, yaw):
 
 def drawRotatedBox(img, x, y, w, l, yaw, color):
     bev_corners = get_corners(x, y, w, l, yaw)
-    print (x, y, w, l)
+    print (x, y, w, l, yaw, color) # bounding box parameters value
     corners_int = bev_corners.reshape(-1, 1, 2).astype(int)
     cv2.polylines(img, [corners_int], True, color, 2)
     corners_int = bev_corners.reshape(-1, 2).astype(int)
