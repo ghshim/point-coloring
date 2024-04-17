4d9cb4e404a44682f5475cc03b00358b61c43d9a
Topasm <dhkdwk0215@naver.com>
2024-04-17 15:06:10 +0900

    test

--- sfa/evaluate.py
+++ sfa/evaluate.py
@@ -1,3 +1,15 @@
+from data_process.kitti_data_utils import Calibration
+from utils.evaluation_utils import decode, post_processing, get_batch_statistics_rotated_bbox, ap_per_class, load_classes, convert_det_to_real_values, decode, post_processing, post_processing_v2, get_batch_statistics_rotated_bbox, ap_per_class, draw_predictions
+from utils.misc import AverageMeter, ProgressMeter, time_synchronized
+from models.model_utils import create_model
+from data_process.kitti_dataloader import create_val_dataloader
+import config.kitti_config as cnf
+from utils.torch_utils import _sigmoid
+from easydict import EasyDict as edict
+from tqdm import tqdm
+import torch.utils.data.distributed
+import torch
+import cv2
 import argparse
 import os
 import time
@@ -6,22 +18,9 @@ import sys
 import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning)
-import cv2
-import torch
-import torch.utils.data.distributed
-from tqdm import tqdm
-from easydict import EasyDict as edict
-from utils.torch_utils import _sigmoid
-import config.kitti_config as cnf
 
 sys.path.append('./')
 
-from data_process.kitti_dataloader import create_val_dataloader
-from models.model_utils import create_model
-from utils.misc import AverageMeter, ProgressMeter, time_synchronized
-from utils.evaluation_utils import decode, post_processing, get_batch_statistics_rotated_bbox, ap_per_class, load_classes, convert_det_to_real_values, decode, post_processing, post_processing_v2, get_batch_statistics_rotated_bbox, ap_per_class, draw_predictions
-
-from data_process.kitti_data_utils import Calibration
 
 def evaluate_mAP(val_loader, model, configs, logger):
     batch_time = AverageMeter('Time', ':6.3f')
@@ -39,7 +38,7 @@ def evaluate_mAP(val_loader, model, configs, logger):
             input_bev_maps = imgs.to(configs.device, non_blocking=True).float()
             t1 = time_synchronized()
             outputs = model(input_bev_maps)
-            
+
             # Extract labels
             outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
             outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
@@ -47,34 +46,38 @@ def evaluate_mAP(val_loader, model, configs, logger):
             detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
                                 outputs['dim'], K=configs.K)
             detections = detections.cpu().numpy().astype(np.float32)
-            detections = post_processing(detections, configs.num_classes, configs.down_ratio, configs.peak_thresh)
+            detections = post_processing(
+                detections, configs.num_classes, configs.down_ratio, configs.peak_thresh)
             t2 = time_synchronized()
             detections = detections[0]
             # Draw prediction in the image
             print("metadatas: ", metadatas['img_path'])
-    
 
             kitti_dets = convert_det_to_real_values(detections)
-            
+
+            print("targets: ", targets['hm_cen'].shape)
+            print("det: ", detections.shape)
+            print("kitti_dets: ", kitti_dets)
             print("output: ", detections)
 
-           
-            
             # labels += targets[:, 1].tolist()
             # print("labels: ", labels)
 
-
-            sample_metrics += get_batch_statistics_rotated_bbox(outputs, targets, iou_threshold=configs.iou_thresh)
-
+            sample_metrics += get_batch_statistics_rotated_bbox(
+                outputs, targets, iou_threshold=configs.iou_thresh)
 
         # Concatenate sample statistics
-        true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
-        precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
+        true_positives, pred_scores, pred_labels = [
+            np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+        precision, recall, AP, f1, ap_class = ap_per_class(
+            true_positives, pred_scores, pred_labels, labels)
 
     return precision, recall, AP, f1, ap_class
 
+
 def parse_eval_configs():
-    parser = argparse.ArgumentParser(description='Demonstration config for Complex YOLO Implementation')
+    parser = argparse.ArgumentParser(
+        description='Demonstration config for Complex YOLO Implementation')
     parser.add_argument('--classnames-infor-path', type=str, default='../../dataset/kitti/classes_names.txt',
                         metavar='PATH', help='The class names of objects in the task')
     parser.add_argument('-a', '--arch', type=str, default='darknet', metavar='ARCH',
@@ -107,7 +110,7 @@ def parse_eval_configs():
                         help='for evaluation - the threshold for nms')
     parser.add_argument('--iou-thresh', type=float, default=0.5,
                         help='for evaluation - the threshold for IoU')
-    
+
     parser.add_argument('--K', type=int, default=50,
                         help='the number of top K')
 
@@ -115,7 +118,7 @@ def parse_eval_configs():
     configs.pin_memory = True
 
     ####################################################################
-    ##############Dataset, Checkpoints, and results dir configs#########
+    ############## Dataset, Checkpoints, and results dir configs#########
     ####################################################################
     configs.working_dir = '../'
     configs.dataset_dir = os.path.join(configs.working_dir, 'dataset', 'kitti')
@@ -147,7 +150,8 @@ def parse_eval_configs():
         'dim': configs.num_dim
     }
     configs.num_input_features = 4
-    configs.dataset_dir = os.path.join('/home/ahrilab/PrincipleDL/dataset', 'kitti')
+    configs.dataset_dir = os.path.join(
+        '/home/ahrilab/PrincipleDL/dataset', 'kitti')
     return configs
 
 
@@ -159,11 +163,13 @@ if __name__ == '__main__':
     model = create_model(configs)
     # model.print_network()
     print('\n\n' + '-*=' * 30 + '\n\n')
-    configs.device = torch.device('cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx))
-    assert os.path.isfile(configs.pretrained_path), "No file at {}".format(configs.pretrained_path)
-    model.load_state_dict(torch.load(configs.pretrained_path, map_location='cpu'), strict=False)
+    configs.device = torch.device(
+        'cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx))
+    assert os.path.isfile(configs.pretrained_path), "No file at {}".format(
+        configs.pretrained_path)
+    model.load_state_dict(torch.load(
+        configs.pretrained_path, map_location='cpu'), strict=False)
 
-    
     model = model.to(device=configs.device)
 
     model.eval()
@@ -171,10 +177,11 @@ if __name__ == '__main__':
     val_dataloader = create_val_dataloader(configs)
 
     print("\nStart computing mAP...\n")
-    precision, recall, AP, f1, ap_class = evaluate_mAP(val_dataloader, model, configs, None)
+    precision, recall, AP, f1, ap_class = evaluate_mAP(
+        val_dataloader, model, configs, None)
     print("\nDone computing mAP...\n")
     for idx, cls in enumerate(ap_class):
-        print("\t>>>\t Class {} ({}): precision = {:.4f}, recall = {:.4f}, AP = {:.4f}, f1: {:.4f}".format(cls, \
-                class_names[cls][:3], precision[idx], recall[idx], AP[idx], f1[idx]))
+        print("\t>>>\t Class {} ({}): precision = {:.4f}, recall = {:.4f}, AP = {:.4f}, f1: {:.4f}".format(cls,
+                                                                                                           class_names[cls][:3], precision[idx], recall[idx], AP[idx], f1[idx]))
 
     print("\nmAP: {}\n".format(AP.mean()))
